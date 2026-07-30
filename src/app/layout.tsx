243c007dbcd0aea35c4bf1e9d0d219b4e36f6233
zeroxawm-gif <zeroxawm@gmail.com>
2026-07-31 00:37:05 +0700

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="id">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
